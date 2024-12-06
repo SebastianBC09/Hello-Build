d0cd6cd995942b663b00199793c5375f71bcb5c1
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-06 18:11:28 -0500

    feat: configure provider wrapper components
    - Set up root provider structure
    - Add React Router configuration
    - Implement provider composition order

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Auth0ProviderWithHistory from './auth/provider/AuthProvider.tsx'
+import ApolloProviderWithConfig from './apollo/provider/ApolloProvider.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0ProviderWithHistory>
+      <ApolloProviderWithConfig>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProviderWithConfig>
+    </Auth0ProviderWithHistory>
   </StrictMode>,
 )
