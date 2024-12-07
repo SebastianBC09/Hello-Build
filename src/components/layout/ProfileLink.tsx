2c001633b855e79bff8e49f845dcb47839e5f048
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-07 09:07:07 -0500

    feat: implement profile navigation component
    - Add avatar and name display
    - Create route link to profile
    - Style user profile section

---
+++ src/components/layout/ProfileLink.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Typography } from "@mui/material";
+
+const ProfileLink: React.FC = () => {
+  return (
+    <Link to="/profile" style={{ textDecoration: "none", color: "inherit" }}>
+      <Avatar alt={name} src={picture} />
+      <Typography variant="body1" component="div" sx={{ ml: 1 }}>
+        {name}
+      </Typography>
+    </Link>
+  );
+};
+
+export default ProfileLink;
