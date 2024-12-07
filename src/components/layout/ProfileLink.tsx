5961db60f87defbee57cd9393329031a52d26137
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-07 17:17:40 -0500

    component fixed

--- src/components/layout/ProfileLink.tsx
+++ src/components/layout/ProfileLink.tsx
@@ -1,14 +1,24 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { Avatar, Typography } from "@mui/material";
+import { Avatar, Typography, Box } from "@mui/material";
+import { useAuth } from "../../auth/hooks/useAuth";
 
 const ProfileLink: FC = () => {
+  const { user } = useAuth();
+
+  if (!user) return null;
+
   return (
     <Link to="/profile" style={{ textDecoration: "none", color: "inherit" }}>
-      <Avatar alt={name} src={picture} />
-      <Typography variant="body1" component="div" sx={{ ml: 1 }}>
-        {name}
-      </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar
+          alt={user.name || user.nickname}
+          src={user.picture}
+        />
+        <Typography variant="body1" sx={{ ml: 1 }}>
+          {user.name || user.nickname}
+        </Typography>
+      </Box>
     </Link>
   );
 };
