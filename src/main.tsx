1359b224efd8aa9a47531c2af66d82e828ea883d
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-08 23:43:31 -0500

    refactor: remove Apollo client implementation - Remove Apollo provider - Clean up GraphQL queries - Update service layer to use REST endpoints - Remove unused dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Auth0ProviderWithHistory from './auth/provider/AuthProvider.tsx'
-import ApolloProviderWithConfig from './apollo/provider/ApolloProvider.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Auth0ProviderWithHistory>
-        <ApolloProviderWithConfig>
-          <App />
-        </ApolloProviderWithConfig>
+        <App />
       </Auth0ProviderWithHistory>
     </BrowserRouter>
   </StrictMode>,
